822c6b13bec3cdad3dc0d6004ef68b2cb1c59102
Dakarri Jenkins <dakarrijenkins@gmail.com>
2026-09-03 10:18:09 -0400

    Fixed the message bug for difficulty range and the submit guess button

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if "history" not in st.session_state:
 st.subheader("Make a guess")
 
 st.info(
-    f"Guess a number between 1 and 100. "
+    f"Guess a number between {low} and {high}. "
     f"Attempts left: {attempt_limit - st.session_state.attempts}"
 )
 
@@ -96,13 +96,8 @@ if submit:
         st.error(err)
     else:
         st.session_state.history.append(guess_int)
-        # FIXME: Triggers TypeError
-        if st.session_state.attempts % 2 == 0:
-            secret = str(st.session_state.secret)
-        else:
-            secret = st.session_state.secret
 
-        outcome, message = check_guess(guess_int, secret)
+        outcome, message = check_guess(guess_int, st.session_state.secret)
 
         if show_hint:
             st.warning(message)
